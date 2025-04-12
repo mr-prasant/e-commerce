97b7f48140ac0ebde703413dbc71f4a600282c1c
Brajesh Kumar <kumarbrajeah455@gmail.com>
2025-04-13 05:14:09 +0530

    Implemented formatDateFromMillis method in ServiceUtil

--- E-Commerce/src/main/java/com/ecommerce/utility/ServiceUtil.java
+++ E-Commerce/src/main/java/com/ecommerce/utility/ServiceUtil.java
@@ -2,15 +2,25 @@ package com.ecommerce.utility;
 
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 public class ServiceUtil {
 
+    // generating id
     public String generateId(String prefix) {
         long timestamp = System.currentTimeMillis();
         int randomNumber = ThreadLocalRandom.current().nextInt(100000, 10000001);
 
         return prefix + timestamp + randomNumber;
+    }
+
+    // format date from millisecond
+    public String formatDateFromMillis(long millis) {
+        Date date = new Date(millis);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        return sdf.format(date);
     }
 }
